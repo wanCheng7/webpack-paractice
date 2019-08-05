b4f8293eb36c8a889cd53eb0cff5e3425ce36162
wancheng7 <525918717@qq.com>
2019-08-05 22:49:12 +0800

    init

---
+++ src/search.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import demoImg from './images/timg.jpg';
+import erweimaImg from './images/erweima.png';
+import './search.less';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search-text">
+        <img src={demoImg} />
+        <img src={erweimaImg} alt=""/>
+        <div className="search-text">Search Test</div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Search />,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+
+const path = require('path');
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240  //b
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
